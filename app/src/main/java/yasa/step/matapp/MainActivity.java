8d56dc46caff5d93c93930cd724413b39196702b
Maratik375 <maratsaifutdinov22@gmail.com>
2024-11-05 02:28:37 +0500

    First commit

---
+++ app/src/main/java/yasa/step/matapp/MainActivity.java
@@ -0,0 +1,31 @@
+package yasa.step.matapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// a
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int zagr = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonToActivity3 =(Button)findViewById(R.id.button2);
+        buttonToActivity3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent buttonToActivity3 = new Intent(MainActivity.this, MainActivity3.class);
+        startActivity(buttonToActivity3);
+    }
+}
